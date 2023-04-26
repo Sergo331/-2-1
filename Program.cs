a0f1327c501729767ee340bfde070bea216c5c1e
Sergo331 <proha331@gmail.com>
2023-04-26 18:23:25 +0300

    задача 2-1
    задача 2-1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string massage;
+            int howMuch;
+
+            Console.Write("Введите ваш текст: ");
+            massage = Console.ReadLine();
+
+            Console.Write("Какое колличество раз его повторить?: ");
+            howMuch = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < howMuch; i++) 
+            {
+                Console.WriteLine(massage);
+            }
+
+        }
+    }
+}
